800ab80b92d54b8bddbd5275702a446c9da29d72
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-03-05 12:59:45 +0100

    8th build

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+# mvn -B package
+# cp src/main/docker/Dockerfile target/
+# docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
+# docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
+# docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
+
+
+
 mvn -B package
 cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
-docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
-docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
+docker buildx create  --name multi-arch-builder --platform linux/amd64,linux/arm64
+docker buildx use multi-arch-builder
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest \
+  --push \
+  ./target
