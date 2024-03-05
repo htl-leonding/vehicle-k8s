53ac8ea5e6b19c744470a2ec05a87985594670e7
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-03-05 12:44:13 +0100

    6th build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 mvn -B package
 cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
-docker build --platform linux/arm64 --tag ghcr.io/$GITHUB_REPOSITORY/backend-arm:latest ./target
-docker push ghcr.io/$GITHUB_REPOSITORY/backend-arm:latest
+docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
+docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
